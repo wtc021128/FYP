ecbe13863f91fd6d0bcfeafba500769831db15ad
TKH1021 <97867665+TKH1021@users.noreply.github.com>
2023-10-02 22:30:52 +0800

    Add files via upload

---
+++ FYP/login page/e02_db.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.1
+ */
+
+/**
+ * Database `e02_db`
+ */
+
+/* `e02_db`.`admin` */
+$admin = array(
+  array('adminID' => 'A001','admin_Name' => 'TAM KA HO','password' => 'rootroot','email' => '210069852@stu.vtc.edu.hk','Phone' => NULL),
+  array('adminID' => 'A002','admin_Name' => 'Wong Tsz Chun','password' => 'rootroot','email' => '210075207@stu.vtc.edu.hk','Phone' => NULL),
+  array('adminID' => 'A003','admin_Name' => 'Cheung Chi Him','password' => 'rootroot','email' => '210072739@stu.vtc.edu.hk','Phone' => NULL),
+  array('adminID' => 'A004','admin_Name' => 'Shiu Chung Hei','password' => 'rootroot','email' => '210129496@stu.vtc.edu.hk','Phone' => NULL)
+);
+
+/* `e02_db`.`all_user` */
+$all_user = array(
+);
+
+/* `e02_db`.`assignment` */
+$assignment = array(
+);
+
+/* `e02_db`.`exam` */
+$exam = array(
+);
+
+/* `e02_db`.`lesson` */
+$lesson = array(
+);
+
+/* `e02_db`.`log` */
+$log = array(
+);
+
+/* `e02_db`.`mathtopic` */
+$mathtopic = array(
+);
+
+/* `e02_db`.`questions` */
+$questions = array(
+  array('question_id' => 'q00001','question_type_id' => 'qt001','questionPoint' => '4','question_text' => 'The cost of a chair is $ 360 . If the chair is sold at a discount of 20 % on its marked price, then the percentage profit is 30 % . Find the marked price of the chair.','answer_text' => 'Let $ x be the marked price of the chair. pp−1 for undefined symbol
+x 1( − 20%) = 360 1( + 30%) 1M+1M+1A 1M for x 1( − 20%)
+0.8
+360 )3.1(
+x =
+x = 585 1A
+ Thus, the marked price of the chair is $585 . u−1 for missing unit
+ The marked price of the ch')
+);
+
+/* `e02_db`.`question_type` */
+$question_type = array(
+);
+
+/* `e02_db`.`quiz` */
+$quiz = array(
+);
+
+/* `e02_db`.`quizquestion` */
+$quizquestion = array(
+);
+
+/* `e02_db`.`registered_users` */
+$registered_users = array(
+  array('cna' => 'carlos','username' => 'carlos','email' => 'carlos123@gmail.com','password' => '$2y$10$7302QkjwsOa3oje2HlKFseUgYjTfPaYBNIEjLNdmV5ymXMFm6I6pK')
+);
+
+/* `e02_db`.`student` */
+$student = array(
+);
+
+/* `e02_db`.`studentassignment` */
+$studentassignment = array(
+);
+
+/* `e02_db`.`studentexam` */
+$studentexam = array(
+);
+
+/* `e02_db`.`studentprofile` */
+$studentprofile = array(
+);
+
+/* `e02_db`.`studentprogress` */
+$studentprogress = array(
+);
+
+/* `e02_db`.`studentquizattempt` */
+$studentquizattempt = array(
+);
+
+/* `e02_db`.`studentregister` */
+$studentregister = array(
+);
+
+/* `e02_db`.`teacher` */
+$teacher = array(
+);
+
+/* `e02_db`.`teacherassignment` */
+$teacherassignment = array(
+);
+
+/* `e02_db`.`teacherprofile` */
+$teacherprofile = array(
+);
+
+/* `e02_db`.`websitestatistics` */
+$websitestatistics = array(
+);

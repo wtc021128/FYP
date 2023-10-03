973684eb63f9457e8c54d2a26731054b8b0a372e
CCH020102 <97867659+CCH020102@users.noreply.github.com>
2023-10-03 14:03:55 +0800

    Add files via upload

--- FYP/htdocs/QA.php
+++ FYP/htdocs/QA.php
@@ -29,7 +29,7 @@
   <td >
   </td>
   <td align="center">
-    <font size="6"><B> 數學/Mathematics </f><B>
+    <font size="6" id="top"><B>數學/Mathematics</f><B>
   </td>
   <td >
   </td>
@@ -38,10 +38,10 @@
   <font size="4"><li><a href="#one">By Topic</a></li></f>
   </td>
   <td align="center" width="400">
-  <font size="4"><a>DSE (中)</f></a>
+  <font size="4"><li><a href="#two">DSE (中)</f></a></li>
   </td>
   <td align="center" width="400">
-  <font size="4"><a>DSE (英)</f></a>
+  <font size="4"><li><a href="#three">DSE (英)</f></a></li>
   </td>
   <tr>
     <td height="30">
@@ -52,13 +52,13 @@
     </td>
 <tr>
   <td align="center">
-  <font size="4"><a>Math&Stat</f></a>
+  <font size="4"><a href="#four">Math&Stat</f></a>
   </td>
   <td align="center">
-  <font size="4"><a>PureMaths</f></a>
+  <font size="4"><a href="#five">PureMaths</f></a>
   </td>
   <td align="center">
-  <font size="4"><a>AddMaths</f></a>
+  <font size="4"><a href="#six">AddMaths</f></a>
   </td>
 </tr>
 <tr>
@@ -103,10 +103,588 @@
   Topic 17 <a href="https://dse.life/static/pp/m0/eng/bytopic/p1/17.pdf">Counting Principles and Probability</a></b> 
   Topic 18 <a href="https://dse.life/static/pp/m0/eng/bytopic/p1/18.pdf">Statistics</a></b>
   <br><a href="https://dse.life/static/pp/m0/eng/bytopic/p1/bk1.pdf">Book 1 Topic 1-18</a></b></f>
-
-
-  
-
-
-
-
+  </td>
+  <tr>
+    <td height="100">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+<Tr align="center">
+  <td>
+  </td>
+    <td align="center">
+      <br id="two">
+      <font size="6">DSE (中)</f><a href="#top"><font size="2">Go to Top</f>
+    </td>
+  <td>
+  </td>
+  <tr>
+    <td height="50">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  <tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>練習紙</f>
+    <br><font size="5"><a href="https://dse.life/static/pp/m0/chi/dse/pp/p1.pdf">練習1</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://dse.life/static/pp/m0/chi/dse/pp/ans.pdf">答案</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  <tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>樣本紙</f>
+    <br><font size="5"><a href="https://dse.life/static/pp/m0/chi/dse/sp/p1.pdf">練習1</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://dse.life/static/pp/m0/chi/dse/sp/ans.pdf">答案</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  <tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2012</f>
+    <br><font size="5"><a href="https://dse.life/static/pp/m0/chi/dse/2012/p1.pdf">練習1</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://dse.life/static/pp/m0/chi/dse/2012/ans.pdf">答案</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  <tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2013</f>
+    <br><font size="5"><a href="https://dse.life/static/pp/m0/chi/dse/2013/p1.pdf">練習1</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://dse.life/static/pp/m0/chi/dse/2013/ans.pdf">答案</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  <tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2014</f>
+    <br><font size="5"><a href="https://dse.life/static/pp/m0/chi/dse/2014/p1.pdf">練習1</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://dse.life/static/pp/m0/chi/dse/2014/ans.pdf">答案</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  <tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2015</f>
+    <br><font size="5"><a href="https://dse.life/static/pp/m0/chi/dse/2015/p1.pdf">練習1</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://dse.life/static/pp/m0/chi/dse/2015/ans.pdf">答案</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  <tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2016</f>
+    <br><font size="5"><a href="https://dse.life/static/pp/m0/chi/dse/2016/p1.pdf">練習1</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://dse.life/static/pp/m0/chi/dse/2016/ans.pdf">答案</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  <tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2017</f>
+    <br><font size="5"><a href="https://dse.life/static/pp/m0/chi/dse/2017/p1.pdf">練習1</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://dse.life/static/pp/m0/chi/dse/2017/ans.pdf">答案</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  <tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2018</f>
+    <br><font size="5"><a href="https://dse.life/static/pp/m0/chi/dse/2018/p1.pdf">練習1</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://dse.life/static/pp/m0/chi/dse/2018/ans.pdf">答案</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  <tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2019</f>
+    <br><font size="5"><a href="https://dse.life/static/pp/m0/chi/dse/2019/p1.pdf">練習1</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://dse.life/static/pp/m0/chi/dse/2019/ans.pdf">答案</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  <tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2020</f>
+    <br><font size="5"><a href="https://dse.life/static/pp/m0/chi/dse/2020/p1.pdf">練習1</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://dse.life/static/pp/m0/chi/dse/2020/ans.pdf">答案</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  <tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2021</f>
+    <br><font size="5"><a href="https://dse.life/static/pp/m0/chi/dse/2021/p1.pdf">練習1</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://dse.life/static/pp/m0/chi/dse/2021/ans.pdf">答案</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  <tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2022</f>
+    <br><font size="5"><a href="https://dse.life/static/pp/m0/chi/dse/2022/p1.pdf">練習1</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://dse.life/static/pp/m0/chi/dse/2022/ans.pdf">答案</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  <tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2023</f>
+    <br><font size="5"><a href="https://dse.life/static/pp/m0/chi/dse/2023/p1.pdf">練習1</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://dse.life/static/pp/m0/chi/dse/2023/ans.pdf">答案</a>
+  </td>
+  <tr>
+    <td height="100">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+<Tr align="center">
+  <td>
+  </td>
+    <td align="center">
+      <br id="three">
+      <font size="6">DSE (英)</f><a href="#top"><font size="2">Go to Top</f>
+    </td>
+  <td>
+  </td>
+  <tr>
+    <td height="50">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  <tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>Practice Paper</f>
+    <br><font size="5"><a href="https://dse.life/static/pp/m0/eng/dse/pp/p1.pdf">Paper1</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://dse.life/static/pp/m0/eng/dse/pp/ans.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+    <tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>Sample Paper</f>
+    <br><font size="5"><a href="https://dse.life/static/pp/m0/eng/dse/sp/p1.pdf">Paper1</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://dse.life/static/pp/m0/eng/dse/sp/ans.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  <tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2012</f>
+    <br><font size="5"><a href="https://dse.life/static/pp/m0/eng/dse/2012/p1.pdf">Paper1</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://dse.life/static/pp/m0/eng/dse/2012/ans.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  <tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2013</f>
+    <br><font size="5"><a href="https://dse.life/static/pp/m0/eng/dse/2013/p1.pdf">Paper1</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://dse.life/static/pp/m0/eng/dse/2013/ans.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  <tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2014</f>
+    <br><font size="5"><a href="https://dse.life/static/pp/m0/eng/dse/2014/p1.pdf">Paper1</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://dse.life/static/pp/m0/eng/dse/2014/ans.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  <tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2015</f>
+    <br><font size="5"><a href="https://dse.life/static/pp/m0/eng/dse/2015/p1.pdf">Paper1</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://dse.life/static/pp/m0/eng/dse/2015/ans.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  <tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2016</f>
+    <br><font size="5"><a href="https://dse.life/static/pp/m0/eng/dse/2016/p1.pdf">Paper1</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://dse.life/static/pp/m0/eng/dse/2016/ans.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  <tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2017</f>
+    <br><font size="5"><a href="https://dse.life/static/pp/m0/eng/dse/2017/p1.pdf">Paper1</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://dse.life/static/pp/m0/eng/dse/2017/ans.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  <tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2018</f>
+    <br><font size="5"><a href="https://dse.life/static/pp/m0/eng/dse/2018/p1.pdf">Paper1</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://dse.life/static/pp/m0/eng/dse/2018/ans.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  <tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2019</f>
+    <br><font size="5"><a href="https://dse.life/static/pp/m0/eng/dse/2019/p1.pdf">Paper1</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://dse.life/static/pp/m0/eng/dse/2019/ans.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  <tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2020</f>
+    <br><font size="5"><a href="https://dse.life/static/pp/m0/eng/dse/2020/p1.pdf">Paper1</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://dse.life/static/pp/m0/eng/dse/2020/ans.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  <tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2021</f>
+    <br><font size="5"><a href="https://dse.life/static/pp/m0/eng/dse/2021/p1.pdf">Paper1</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://dse.life/static/pp/m0/eng/dse/2021/ans.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  <tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2022</f>
+    <br><font size="5"><a href="https://dse.life/static/pp/m0/eng/dse/2022/p1.pdf">Paper1</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://dse.life/static/pp/m0/eng/dse/2022/ans.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  <tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2023</f>
+    <br><font size="5"><a href="https://dse.life/static/pp/m0/eng/dse/2023/p1.pdf">Paper1</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://dse.life/static/pp/m0/eng/dse/2023/ans.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="100">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+<Tr align="center">
+  <td>
+  </td>
+    <td align="center">
+      <br id="four">
+      <font size="6">Mathematics & Statistics</f><a href="#top"><font size="2">Go to Top</f>
+    </td>
+  <td>
+  </td>
+  <tr>
+    <td height="50">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  <tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>1996</f>
+    <br><font size="5"><a href="https://www.dse.life/static/pp/m1/mands/1996.pdf">Question Paper</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://www.dse.life/static/pp/m1/mands/1996ms.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+    <tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>1997</f>
+    <br><font size="5"><a href="https://www.dse.life/static/pp/m1/mands/1997.pdf">Question Paper</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://www.dse.life/static/pp/m1/mands/1997ms.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td><tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>1998</f>
+    <br><font size="5"><a href="https://www.dse.life/static/pp/m1/mands/1998.pdf">Question Paper</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://www.dse.life/static/pp/m1/mands/1998ms.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td><tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>1999</f>
+    <br><font size="5"><a href="https://www.dse.life/static/pp/m1/mands/1999.pdf">Question Paper</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://www.dse.life/static/pp/m1/mands/1999ms.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td><tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2000</f>
+    <br><font size="5"><a href="https://www.dse.life/static/pp/m1/mands/2000.pdf">Question Paper</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://www.dse.life/static/pp/m1/mands/2000ms.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td><tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2001</f>
+    <br><font size="5"><a href="https://www.dse.life/static/pp/m1/mands/2001.pdf">Question Paper</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://www.dse.life/static/pp/m1/mands/2001ms.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td><tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2002</f>
+    <br><font size="5"><a href="https://www.dse.life/static/pp/m1/mands/2002.pdf">Question Paper</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://www.dse.life/static/pp/m1/mands/2002ms.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td><tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2003</f>
+    <br><font size="5"><a href="https://www.dse.life/static/pp/m1/mands/2003.pdf">Question Paper</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://www.dse.life/static/pp/m1/mands/2003ms.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td><tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2004</f>
+    <br><font size="5"><a href="https://www.dse.life/static/pp/m1/mands/2004.pdf">Question Paper</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://www.dse.life/static/pp/m1/mands/2004ms.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td><tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2005</f>
+    <br><font size="5"><a href="https://www.dse.life/static/pp/m1/mands/2005.pdf">Question Paper</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://www.dse.life/static/pp/m1/mands/2005ms.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td><tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2006</f>
+    <br><font size="5"><a href="https://www.dse.life/static/pp/m1/mands/2006.pdf">Question Paper</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://www.dse.life/static/pp/m1/mands/2006ms.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td><tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2007</f>
+    <br><font size="5"><a href="https://www.dse.life/static/pp/m1/mands/2007.pdf">Question Paper</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://www.dse.life/static/pp/m1/mands/2007ms.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td><tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2008</f>
+    <br><font size="5"><a href="https://www.dse.life/static/pp/m1/mands/2008.pdf">Question Paper</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://www.dse.life/static/pp/m1/mands/2008ms.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td><tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2009</f>
+    <br><font size="5"><a href="https://www.dse.life/static/pp/m1/mands/2009.pdf">Question Paper</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://www.dse.life/static/pp/m1/mands/2009ms.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td><tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2010</f>
+    <br><font size="5"><a href="https://www.dse.life/static/pp/m1/mands/2010.pdf">Question Paper</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://www.dse.life/static/pp/m1/mands/2010ms.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td><tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2011</f>
+    <br><font size="5"><a href="https://www.dse.life/static/pp/m1/mands/2011.pdf">Question Paper</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://www.dse.life/static/pp/m1/mands/2011ms.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td><tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2012</f>
+    <br><font size="5"><a href="https://www.dse.life/static/pp/m1/mands/2012.pdf">Question Paper</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://www.dse.life/static/pp/m1/mands/2012ms.pdf">Answer</a>
+  </td>
+  <tr>
+    <td height="25">
+    </td>
+    <td>
+    </td>
+    <td>
+    </td><tr>
+  <td colspan="3" class="black-solid-border"><font size="6"><B>2013</f>
+    <br><font size="5"><a href="https://www.dse.life/static/pp/m1/mands/2013.pdf">Question Paper</a>
+    &nbsp; &nbsp; &nbsp; <a href="https://www.dse.life/static/pp/m1/mands/2013ms.pdf">Answer</a>
+  </td>

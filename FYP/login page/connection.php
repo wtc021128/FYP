617096c40513d2247483c63949791a5b75357e70
TKH1021 <97867665+TKH1021@users.noreply.github.com>
2023-10-03 17:17:08 +0800

    Delete FYP/login page/connection.php

--- FYP/login page/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $con=mysqli_connect("localhost","root","W@2915djkq#a","e02_db") or die("Unable to connect");
-
-    if(mysqli_connect_error()){
-        echo"<script>alert('connect connect to the database');</script>";
-        exit();
-    }
-?>

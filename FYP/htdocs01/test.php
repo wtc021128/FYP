18e709465b3047bd87cd2114a9b70620836a2042
CCH020102 <97867659+CCH020102@users.noreply.github.com>
2023-11-09 10:19:36 +0800

    Add files via upload

--- FYP/htdocs01/test.php
+++ FYP/htdocs01/test.php
@@ -158,13 +158,35 @@ if(isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true)
 </head>
 
 <body>
+  <table border="0">
+    <tr>
+      <td width="400">
+      </td>
+      <td colspan="3" align="center" width="400">
+      <font color="00000" size"8"><B><U>數學科選擇題練習</font>
+  </td>
+  <td width="400">
+  </td>
+  </tr>
+  <td>
+  <font color="00000"><B>﹙請選擇想要進行練習的題數﹚</font>
+  </td>
+  <tr>
+    <td>
     <div class="container">
         <form action="quiz.php" method="POST">
-            <label for="num_questions">要做多少題：</label>
+            <label for="num_questions"><B>請選擇題目數量：</label>
             <input type="number" name="num_questions" min="1" max="10" required>
             <br>
             <input type="submit" value="開始出題">
         </form>
     </div>
+  </td>
+  <td width="400">
+      </td>
+      <td width="400">
+      <font color="00000" size="5">本網站提供不同的數學選擇題練習，<br>練習完成後將會有提供正確答案和成績。請盡力完成練習！</font>
+      </td>
+  </tr>
 </body>
 </html>

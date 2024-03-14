0f7abad5bea2b5a077a54393c7636faca9ec1d9c
CCH020102 <97867659+CCH020102@users.noreply.github.com>
2024-03-14 11:07:42 +0800

    Delete FYP_NEW/assets directory

--- FYP_NEW/assets/js/Home.js
+++
@@ -1,7 +0,0 @@
-/* show YouTube voided */
-$('.youtube-play')[0].addEventListener('click', function(){
-    $('.youtube')[0].style.display = "inline";
-})
-$('.youtube')[0].addEventListener('click', function(){
-    $('.youtube')[0].style.display = "none";
-})

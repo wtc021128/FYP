51cb207abb201efe3f1de067f3f7184f63e6143b
CCH020102 <97867659+CCH020102@users.noreply.github.com>
2024-03-24 16:47:15 +0800

    Delete FYP_NEW directory

--- FYP_NEW/assets/js/Course.js
+++
@@ -1,8 +0,0 @@
-$('button').forEach(function(elm){
-    elm.addEventListener('click', function(e){
-        /* jump to Contact_us page */
-        console.log(e.target.dataset.course)
-        sessionStorage.setItem("course", e.target.dataset.course);
-        window.location.href = "Contact_us.html"
-    })
-})

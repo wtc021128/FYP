d717d68bc23e578a4f567d98ab5913081b233170
CCH020102 <97867659+CCH020102@users.noreply.github.com>
2024-03-24 16:46:53 +0800

    Delete FYP directory

--- FYP/login page/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$con=mysqli_connect("localhost","root","","e02_db") or die("Unable to connect");
-
-if(mysqli_connect_error()){
-    echo "<script>alert('Unable to connect to the database');</script>";
-    exit();
-}
-?>
